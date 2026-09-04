a06aed6d303020d6ac167e59b0a8f31e74945eee
anushkapateriya <anushkapateriya05@gmail.com>
2026-09-04 07:12:51 +0530

    add html form with sendFile

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
-const productController=require('../controllers/productController')
+
+const productController = require('../controllers/productController');
 
 // GET /products
 router.get('/', productController.getAllProducts);
@@ -10,21 +12,5 @@ router.get('/:id', productController.getProductById);
 
 // POST /products
 router.post('/', productController.addProduct);
-
-//router.put('/:id', productController.editProducts);
-
-//router.delete('/:id', productController.deleteProducts);
-router.get('/', productController.getProducts);
-
-// GET /products/:id
-router.get('/:id', productController.getProductsByID);
-
-// POST /products
-router.post('/', productController.postProducts);
-
-router.put('/:id', productController.editProducts);
-
-router.delete('/:id', productController.deleteProducts);
-
 
 module.exports = router;
